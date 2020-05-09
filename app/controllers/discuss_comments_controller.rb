2447bb36a540de41fedbb0a8329147f52108c3ca
sekine11 <sekine11665@gmail.com>
2020-05-09 13:03:02 +0900

    [Add] questions and discussions

--- app/controllers/discuss_comments_controller.rb
+++ app/controllers/discuss_comments_controller.rb
@@ -1,7 +1,23 @@
 class DiscussCommentsController < ApplicationController
   def create
+  	discussion = Discussion.find(params[:discussion_id])
+  	discuss_comment = DiscussComment.new(discuss_comment_params)
+    discuss_comment.user_id = current_user.id
+  	discuss_comment.discussion_id = discussion.id
+  	discuss_comment.save
+  	redirect_to discussion
   end
 
   def destroy
+  	discussion = Discussion.find(params[:discussion_id])
+  	discuss_comment = DiscussComment.find(params[:id])
+    discuss_comment.destroy
+    redirect_to discussion
   end
+
+  private
+  def discuss_comment_params
+    params.require(:discuss_comment).permit(:content)
+  end
+
 end
