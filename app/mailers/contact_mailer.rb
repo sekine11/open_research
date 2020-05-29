76268f63f6b97d3ab14819aeaf26293d14fe036a
sekine11 <sekine11665@gmail.com>
2020-05-29 14:15:49 +0900

    [Add] deploy settings

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -6,7 +6,6 @@ class ContactMailer < ApplicationMailer
   #
   def contact_mail(contact)
     @contact = contact
-
-    mail to: ENV['MAIL'], subject: "お問い合わせ"
+    mail to: ENV['EMAIL'], subject: "お問い合わせ"
   end
 end
